e892448eaaecd0320662a5859c1b0934d2f1f236
John Millner <millnerjohn@gmail.com>
2017-12-10 17:52:38 -0500

    adding in custom gps_goal package

---
+++ catkin_ws/src/eel4660/src/translater.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+import rospy
+
+from sensor_msgs.msg import NavSatFix
+
+def callback(data):
+    
+    #set up publisher
+    pub = rospy.Publisher('gps_goal_fix', NavSatFix, queue_size=10)
+    rate = rospy.Rate(10)
+    
+    #translate
+    pub.publish(data)
+    rate.sleep()
+    
+    
+def translator():
+
+    #start up node
+    rospy.init_node('translator',anonymous=True)
+
+    
+    
+    #set up subscriber
+    rospy.Subscriber('fix', NavSatFix, callback)
+    
+    #enter loop 
+    rospy.spin()
+        
+        
+
+
+if __name__ == '__main__':
+    try:
+        translator()
+    except rospy.ROSInterruptException:
+        pass
