a295f6eddb619e7e44cb49d4b8e57460379570cf
Richie Wales <richard_wales@knights.ucf.edu>
2018-03-28 19:10:53 -0400

    Moving gait movement to arduino, pi just sends gait over spi

--- Gait/ga.py
+++ Gait/ga.py
@@ -1,7 +1,7 @@
 import random
 import ConfigParser
 import rospy
-from gait import *
+import json
 
 """
 Things need to discuss:
@@ -11,6 +11,107 @@ ROS topic and message details for sending the gait
 Test procedure for fitness evaluation (move straight line, whatever)
 Judging stability
 """
+
+class Leg:
+    def __init__(self, states):
+        self.states = states
+
+class Step:
+    def __init__(self, legs=None, angle_min=None, angle_max=None):
+        if legs == None:
+            self.generate_legs(angle_min, angle_max)
+        else:
+            self.legs = legs
+
+    def generate_legs(self, angle_min, angle_max):
+        self.states = []
+
+        for i in range(6):
+            for j in range(3):
+
+                # min/max angles for each servo can be different, according to config
+                # i*3 + j gets us a value from [0, 17] to access a value for our 18 servos
+                self.states.append(
+                    Leg(
+                        [random.rantint(angle_min[i*3 + j], angle_max[i*3 + j])]
+                    )
+                )
+
+@total_ordering
+class Gait:
+    def __init__(self, steps=None, num_steps=None, angle_min=None, angle_max=None):
+        if steps == None:
+            self.generate_steps(num_steps, angle_min, angle_max)
+        else:
+            self.steps = steps
+
+        self.fitness = 0
+
+    def generate_steps(self, num_steps, angle_min, angle_max):
+        self.steps = [Step(angle_min, angle_max) for i in range(num_steps)]
+        
+    def __eq__(self, other):
+        return hasattr(other, 'fitness') and self.fitness == other.fitness
+
+    def __lt__(self, other):
+        return hasattr(other, 'fitness') and self.fitness < other.fitness
+
+class MCU:
+    def __init__(self, gait=None):
+        self.gait = gait
+        self.pi = pigpio.pi()
+        
+        # Open SPI 0 (idk why, thats the one) at 115200 baud
+        self.spi = pi.spi_open(0, 115200)
+        
+    def send_gait_mcu(self):
+        """
+        Sample Arduino SPI code RX
+
+        char step = 0;
+        char leg = 0;
+        char servo = 0;
+
+        int[][][] gait = new int[12][6][3];
+
+        // SPI interrupt routine
+        ISR (SPI_STC_vect)
+        {
+            // grab byte from SPI Data Register
+            byte data = SPDR;
+            
+            char num_servos_set = (step * servo * leg + leg * servo + servo);
+
+            // Add to gait if we haven't set everything
+            if (num_servos_set < sizeof gait)
+            {
+                gait[step][leg][servo] = data;
+
+                servo++;
+
+                if (servo > 2)
+                {
+                    servo = 0;
+                    leg++;
+
+                    if (leg > 5)
+                    {
+                        step++;
+                    }
+                }
+            }
+        }
+        """
+        
+        # Send each servo state for all possible 216 states (3 servos * 6 legs * 12 steps)
+        for step in self.gait.steps:
+            for leg in step.legs:
+                for state in leg.states:
+                    (count, rx_data) = self.pi.spi_xfer(self.spi, state)
+
+class Leg:
+    def __init__(self, legs)
+        self.legs = legs
 
 class Config:
     def __init__(self, filename):
@@ -31,8 +132,8 @@ class Config:
         self.mutation_change = cfgParser.getint('GA', 'mutation_change')
 
         self.num_steps = cfgParser.getint('Population', 'num_steps')
-        self.angle_min = cfgParser.getint('Population', 'angle_min')
-        self.angle_max = cfgParser.getint('Population', 'angle_max')
+        self.angle_min = json.loads(cfgParser.get('Population', 'angle_min'))
+        self.angle_max = json.loads(cfgParser.get('Population', 'angle_max'))
 
         self.node_name = cfgParser.get('ROS', 'node_name')
 
