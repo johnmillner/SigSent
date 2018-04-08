0ca83b74baa8179ca15a383a2bd3cccb37fcda38
Richie Wales <richard_wales@knights.ucf.edu>
2018-04-08 17:51:52 -0400

    Code fixes on lightbar

--- catkin_ws/src/sigsent/src/lightbar.py
+++ catkin_ws/src/sigsent/src/lightbar.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import rospy
 import pigpio
 from std_msgs.msg import Int8
@@ -5,13 +6,13 @@ from std_msgs.msg import Int8
 class Lightbar:
     def __init__(self):
         rospy.init_node('lightbar')
-        self.blind_sub = rospy.Subscriber('blind', Int8, callback)
+        self.blind_sub = rospy.Subscriber('blind', Int8, self.callback)
 
         # BCM17
         self.light_pin = 11 
 
         self.pi = pigpio.pi()
-        self.pi.set_mode(light_pin, pigpio.OUTPUT)
+        self.pi.set_mode(self.light_pin, pigpio.OUTPUT)
 
         self.flash_rate = rospy.Rate(10)
         self.flash_count = 6
@@ -19,17 +20,17 @@ class Lightbar:
 
     def callback(self, msg):
         if msg.data == 0:
-            pi.write(light_pin, 0)
-            current_state = 0
+            self.pi.write(self.light_pin, 0)
+            self.current_state = 0
         elif msg.data == 1:
-            pi.write(light_pin, 1)
-            current_state = 1
+            self.pi.write(self.light_pin, 1)
+            self.current_state = 1
         else:
-            for i in range(flash_count):
-                current_state ^= 1
-                pi.write(light_pin, current_state)
+            for i in range(self.flash_count):
+                self.current_state ^= 1
+                self.pi.write(self.light_pin, self.current_state)
                 
-                flash_rate.sleep()
+                self.flash_rate.sleep()
 
 if __name__ == '__main__':
     try:
