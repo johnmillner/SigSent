15a11bf0de9191f1031886c367db17f0f77339a4
Richie Wales <richard_wales@knights.ucf.edu>
2018-04-09 19:02:15 -0400

    Revert "Test functions between arduino and pi"
    This reverts commit 92458918cd8917b6c6b4be9135e0a518ac28b39e.

--- Gait/spi_test.py
+++ Gait/spi_test.py
@@ -7,22 +7,17 @@ from time import sleep
 """
 main to test the message sending. These should be called from a ROS py file
 that needs to...
-<<<<<<< HEAD
 
 Change mode: Button in GUI, publishes message
 TeleOp/Move base: publishes to topics. Listen to them and send the appropriate SPI
 """
 if __name__ == '__main__':
-	"""
-	Change mode: Button in GUI, publishes message
-	TeleOp/Move base: publishes to topics. Listen to them and send the appropriate SPI
-	"""
-if __name__ == '__main__':
+
     pi = pigpio.pi()
     spi = pi.spi_open(0, 115200)
     tests = []
     test_message = Message()
-
+    
     tests.append(test_message.create_walking_message(fwd=True))
     tests.append(test_message.create_walking_message(left=True))
     tests.append(test_message.create_walking_message(right=True))
@@ -35,5 +30,6 @@ if __name__ == '__main__':
 
     for test in tests:
         print('Sending test: {}'.format(test))
+
         pi.spi_xfer(spi, test)
         sleep(1)
