20bed9cbe3345081940b4887f4064a451f707dab
Richie Wales <richard_wales@knights.ucf.edu>
2018-04-17 21:53:56 -0400

    Brought LB back

--- catkin_ws/src/sigsent/src/lightbar.py
+++ catkin_ws/src/sigsent/src/lightbar.py
@@ -13,29 +13,31 @@ class Lightbar:
 
         self.pi = pigpio.pi()
         self.pi.set_mode(self.light_pin, pigpio.OUTPUT)
-
-        self.flash_rate = rospy.Rate(10)
-        self.flash_count = 6
+	self.strobe = False
+        self.flash_rate = rospy.Rate(50)
         self.current_state = 0        
 
     def callback(self, msg):
         if msg.data == 0:
+            self.strobe = False
             self.pi.write(self.light_pin, 0)
             self.current_state = 0
         elif msg.data == 1:
+	    self.strobe = False
             self.pi.write(self.light_pin, 1)
             self.current_state = 1
         else:
-            for i in range(self.flash_count):
-                self.current_state ^= 1
-                self.pi.write(self.light_pin, self.current_state)
+	    self.strobe = True
+            self.current_state ^= 1
                 
-                self.flash_rate.sleep()
 
 if __name__ == '__main__':
     try:
         lb = Lightbar()
-
-        rospy.spin()
+	while not rospy.is_shutdown():
+            while lb.strobe == True:
+	    	lb.pi.write(lb.light_pin, lb.current_state)
+		lb.current_state ^= 1
+	    	lb.flash_rate.sleep()
     except rospy.ROSInterruptException:
         pass
